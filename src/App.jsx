b8b3e55246bfce98e3e3f8d28fb48e16e7999315
Edson | Ghelplus <edson@ghelplus.com.br>
2026-05-21 16:58:41 -0300

    atual

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
-import Dashboard from "./pages/Dashboard";
-import TVMode from "./pages/TVMode";
-
-function App() {
-
-  return (
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route
-          path="/"
-          element={<Dashboard />}
-        />
-
-        <Route
-          path="/tv"
-          element={<TVMode />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
-
-  );
+{
+  "rewrites": [
+    {
+      "source": "/(.*)",
+      "destination": "/"
+    }
+  ]
 }
-
-export default App;
